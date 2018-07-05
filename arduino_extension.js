21469fc6c6297abfba5672c3411992a3126ee852
Gabriel Katz <gabriejk@usc.edu>
2018-07-05 14:40:14 +0300

    changed move motor pin numbers

--- arduino_extension.js
+++ arduino_extension.js
@@ -377,53 +377,53 @@
   {
 	  if (motor_direction === 'stop')
 	  {
-		  analogWrite(3, 0);
 		  analogWrite(5, 0);
-		  analogWrite(6, 0);
+		  analogWrite(7, 0);
+		  analogWrite(2, 0);
+		  analogWrite(3, 0);
 		  analogWrite(9, 0);
 		  analogWrite(10, 0);
-		  analogWrite(11, 0);
 	  }
-	  //motor A: pin 3 and 5
-	  //motor B: pin 6 and 9
-	  //motor C: pin 10 and 11
+	  //motor A: pin 5 and 7
+	  //motor B: pin 2 and 3
+	  //motor C: pin 9 and 10
 	  if (motor_choice === 'motor A')
 	  {
 		  if (motor_direction === 'clockwise')
 		  {
-			  analogWrite(3, speed);
-			  analogWrite(5, 0);
+			  analogWrite(7, 0);
+			  analogWrite(5, speed);
 		  }
 		  else if (motor_direction === 'counterclockwise')
 		  {
-			  analogWrite(3, 0);
-			  analogWrite(5, speed);
+			  analogWrite(5, 0);
+			  analogWrite(7, speed);
 		  }
 	  }
 	  else if (motor_choice === 'motor B')
 	  {
 		  if (motor_direction === 'clockwise')
 		  {
-			  analogWrite(6, speed);
-			  analogWrite(9, 0);
+			  analogWrite(3, 0);
+			  analogWrite(2, speed);
 		  }
 		  else if (motor_direction === 'counterclockwise')
 		  {
-			  analogWrite(6, 0);
-			  analogWrite(9, speed);
+			  analogWrite(2, 0);
+			  analogWrite(3, speed);
 		  }
 	  }
 	  else if (motor_choice === 'motor C')
 	  {
 		  if (motor_direction === 'clockwise')
 		  {
-			  analogWrite(10, speed);
-			  analogWrite(11, 0);
+			  analogWrite(10, 0);
+			  analogWrite(9, speed);
 		  }
 		  else if (motor_direction === 'counterclockwise')
 		  {
-			  analogWrite(10, 0);
-			  analogWrite(11, speed);
+			  analogWrite(9, 0);
+			  analogWrite(10, speed);
 		  }
 	  }
   };
