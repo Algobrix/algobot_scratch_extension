c76d0c0af5547bb5a7d3fecc7e2c1c28414ab080
Gabriel Katz <gabriejk@usc.edu>
2018-07-18 14:46:59 +0300

    added preferences to colorLED

--- arduino_extension.js
+++ arduino_extension.js
@@ -369,8 +369,16 @@
   
   ext.colorLED = function(led_letter, red, green, blue)
   {
-	  register_neopixel(18, 1);
-	  neopixel(0, 255, 0, 0);
+	if (led_letter === 'led A')
+	{
+		register_neopixel(18, 1);
+		neopixel(0, red, green, blue);
+	}
+	else if (led_letter === 'led B')
+	{
+		register neopixel(19, 1);
+		neopixel(0, red, green, blue);
+	}
   }
   
   ext.move_robot = function(robot_direction, num_steps)
