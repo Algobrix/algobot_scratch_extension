d6f8c30acec938a09abee6fab45a786a1877010d
Gabriel Katz <gabriejk@usc.edu>
2018-07-18 11:10:17 +0300

    add neopixel functions

--- algobot_extension.js
+++ algobot_extension.js
@@ -330,6 +330,30 @@
     device.send(msg.buffer);
   }
 
+  function register_neopixel(pin, count)
+  {
+	var msg = new Uint8Array([
+		START_SYSEX
+		NEOPIXEL_REGISTER,
+		pin,
+		count,
+		END_SYSEX]);
+	device.send(msg.buffer);
+  }
+  
+  function neopixel(index, red, green, blue)
+  {
+	var msg = new Uint8Array([
+		START_SYSEX
+		NEOPIXEL,
+		index,
+		red,
+		green,
+		blue,
+		END_SYSEX]);
+	device.send(msg.buffer);
+  }
+  
   function rotateServo(pin, deg) {
     if (!hasCapability(pin, SERVO)) {
       console.log('ERROR: valid servo pins are ' + pinModes[SERVO].join(', '));
